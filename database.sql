d341d042eddfb7f8c842309610209c22cca6be7a
Amareya Allen-Dabney <a.allendabney@gmail.com>
2019-06-06 12:42:18 -0500

    updated package.json

--- database.sql
+++ database.sql
@@ -8,3 +8,48 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "user_info"(
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "first_name" VARCHAR(100),
+	"last_name" VARCHAR(100),
+	"email" VARCHAR(150),
+	"birthday" DATE
+);
+
+CREATE TABLE "brand" (
+    "id" serial primary key,
+    "brand" varchar(50) not null
+);
+
+CREATE TABLE "style" (
+    "id" serial primary key,
+    "style" varchar(50) not null,
+	"brand_id" INT REFERENCES "brand"
+);
+
+CREATE TABLE "color" (
+    "id" serial primary key,
+    "color" varchar(50) not null
+);
+
+CREATE TABLE "image" (
+    "id" serial primary key,
+    "media_key" varchar(1000) not null
+);
+
+CREATE TABLE "shoe" (
+    "id" serial primary key,
+    "user_id" INT REFERENCES "user",
+	"shoe_name" VARCHAR(100),
+	"brand_id" INT REFERENCES "brand",
+	"style" VARCHAR(100),
+	"color1_id" INT REFERENCES "color"("id"),
+	"color2_id" INT REFERENCES "color"("id"),
+	"story" VARCHAR(1000),
+	"image_id" INT REFERENCES "image"("id"),
+	"date_added" DATE,
+	"last_worn" DATE,
+	"deadstock" BOOLEAN
+);
