5c43c46b0dec61d968ae2dd69b126707a496acd2
Amareya Allen-Dabney <a.allendabney@gmail.com>
2019-06-18 21:02:49 -0500

    added npm to package.json

---
+++ build/precache-manifest.21f3883353a2ab17d115c833cf819d8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b417bb6a717de54eb862",
+    "url": "/static/css/main.5b9254b5.chunk.css"
+  },
+  {
+    "revision": "b417bb6a717de54eb862",
+    "url": "/static/js/main.8cdc53b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dcda010f101cb0c8ea15",
+    "url": "/static/js/2.364f43bb.chunk.js"
+  },
+  {
+    "revision": "9b9b22036f20551f4b114d32f468bdee",
+    "url": "/index.html"
+  }
+];
